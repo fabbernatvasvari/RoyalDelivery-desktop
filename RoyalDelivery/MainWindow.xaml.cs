5c8229e3a88eb6675ebc11f9d93ad4340a76f8ff
fabbernatvasvari <sze4fabber@vasvari.org>
2026-01-28 14:44:44 +0100

    Most már a rendelés megjelenik, rendes MVVM CT bindinggal

--- RoyalDelivery/MainWindow.xaml.cs
+++ RoyalDelivery/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace RoyalDelivery
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new ViewModels.MainWindowViewModel();
         }
     }
 }
---
+++ RoyalDelivery/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Controls;
+
+namespace RoyalDelivery.ViewModels
+{
+    public partial class MainWindowViewModel: ObservableObject
+    {
+        // Example: list of orders
+        [ObservableProperty]
+        private ObservableCollection<string> orders;
+
+        // Example: selected order
+        [ObservableProperty]
+        private string selectedOrder;
+
+        // Example: button command
+        [RelayCommand]
+        private void AddOrder()
+        {
+            Orders.Add($"Rendelés #{Orders.Count + 1}");
+        }
+
+        public MainWindowViewModel()
+        {
+            Orders = new ObservableCollection<string>()
+            {
+                "Rendelés #101",
+                "Rendelés #102",
+                "Rendelés #103"
+            };
+            SelectedOrder = Orders[0];
+        }
+    }
+}
