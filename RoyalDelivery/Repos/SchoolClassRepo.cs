4d2d92a8f177ef419ef0e2cf3a8f23ec19880279
fabbernatvasvari <sze4fabber@vasvari.org>
2026-01-29 09:51:22 +0100

    Csaba kódjainak hozzáadása

---
+++ RoyalDelivery/Repos/SchoolClassRepo.cs
@@ -0,0 +1,29 @@
+﻿using MyApp.Desktop.Models;
+
+namespace MyApp.Desktop.Repos
+{
+    /// <summary>
+    /// Repo az iskolai osztályok tárolására
+    /// </summary>
+    public class SchoolClassRepo
+    {
+        /// <summary>
+        /// Memóriában tárolt iskolai osztályok (tesztadatok)
+        /// </summary>
+        private List<SchoolClass> _items = new()
+        {
+            new SchoolClass(9,"a", 26),
+            new SchoolClass(9,"b", 0),
+            new SchoolClass(9,"c", 24)
+        };
+
+        /// <summary>
+        /// Összes iskolai osztály csak olvasható listáját visszaadó metódus
+        /// </summary>
+        /// <returns></returns>
+        public IReadOnlyList<SchoolClass> GetAll()
+        {
+            return _items.ToList();
+        }
+    }
+}
---
+++ RoyalDelivery/ViewModels/SchoolClassViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MyApp.Desktop.Models;
+using MyApp.Desktop.Repos;
+using System.Collections.ObjectModel;
+
+namespace MyApp.Desktop.ViewModels
+{
+    /// <summary>
+    /// MainViewModel a View-n megjelenendő adatokat és interakciókat kezeli
+    /// </summary>
+    public partial class SchoolClassViewModel : ObservableObject
+    {
+        /// <summary>
+        /// A ViewModel-nek része a repo, így eléri a repóban lévő adatokat
+        /// </summary>
+        private readonly SchoolClassRepo _repo = new SchoolClassRepo();
+
+        /// <summary>
+        /// Hogy a View-n megjelenjenek az iskolai osztályok, a ViewModel biztosít egy property-t
+        /// </summary>
+        public ObservableCollection<SchoolClass> Classes { get; }
+
+        public SchoolClassViewModel()
+        {
+            // Az iskolai osztályok a repóból kerülnek betöltésre
+            Classes = new ObservableCollection<SchoolClass>(_repo.GetAll());
+        }
+    }
+}
