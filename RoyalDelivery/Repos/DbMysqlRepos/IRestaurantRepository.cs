c8654f09deeb1de8f8814cae360dfa0d4b9caae3
fabbernatvasvari <sze4fabber@vasvari.org>
2026-03-03 16:00:12 +0100

    nullpointerexceptiont kapok, de nem tudom, hogy miért

---
+++ RoyalDelivery/Repos/DbMysqlRepos/IRestaurantRepository.cs
@@ -0,0 +1,28 @@
+﻿using RoyalDelivery.Models.DbMysqlModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoyalDelivery.Repos.DbMysqlRepos
+{
+    public class IRestaurantRepository
+    {
+        private readonly List<Restaurant> _items;
+
+        public List<Restaurant> GetAll()
+        {
+            return _items.ToList();
+        }
+
+        public void Remove(int selectedId)
+        {
+            Console.WriteLine("public void Remove(Meal SelectedMeal) meghívva.");
+
+            _items.RemoveAt(selectedId);
+        }
+        public void Add(Restaurant restaurant)
+        {
+            _items.Add(restaurant);
+        }
+    }
+}
