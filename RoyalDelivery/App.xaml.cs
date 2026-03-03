8d960d92278d317b510fdd133fe6c80402b60f38
fabbernatvasvari <sze4fabber@vasvari.org>
2026-03-03 11:11:48 +0100

    Now the app starts via DI.

--- RoyalDelivery/App.xaml.cs
+++ RoyalDelivery/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using RoyalDelivery.ViewModels;
+using System;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +12,35 @@ namespace RoyalDelivery
     /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var services = new ServiceCollection();
+
+            ConfigureServices(services);
+
+            ServiceProvider = services.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            // ViewModels
+            services.AddSingleton<MainWindowViewModel>();
+            services.AddTransient<RestaurantViewModel>();
+            services.AddTransient<MealViewModel>();
+            services.AddTransient<OrderViewModel>();
+            services.AddTransient<UserViewModel>();
+
+            // Views
+            services.AddSingleton<MainWindow>();
+
+            // Services & Repositories (example)
+            // services.AddSingleton<IMessageRepository, MessageRepository>();
+        }
     }
 
 }
