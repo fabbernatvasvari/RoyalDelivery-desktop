a35268ed0b4a6d49fc42881783ca5bd6191bdd42
fabbernatvasvari <sze4fabber@vasvari.org>
2026-03-04 17:04:25 +0100

    a radiobuttonok nem akarnak összejönni, lehet, hogy revertelni kell

---
+++ RoyalDelivery/Converters/EnumToBooleanConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RoyalDelivery.Converters;
+public class EnumToBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value?.ToString() == parameter?.ToString();
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null) return null;
+        return (bool)value ? Enum.Parse(targetType, parameter.ToString()) : null;
+    }
+}
