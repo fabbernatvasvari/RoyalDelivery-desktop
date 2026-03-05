21dfc3d82c744a8ea098df918a05d409b0b9e2fc
fabbernatvasvari <sze4fabber@vasvari.org>
2026-03-05 09:17:15 +0100

    VÉGRE MŰKÖDIK!!! Radiobutton implementálca, 3 választási opció. Ebben a ver

--- RoyalDelivery/Converters/EnumToBooleanConverter.cs
+++ RoyalDelivery/Converters/EnumToBooleanConverter.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Windows.Data;
 
 namespace RoyalDelivery.Converters;
+
 public class EnumToBooleanConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
