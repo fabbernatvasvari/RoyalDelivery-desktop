30f9f51e0e42f7726750ce01b045792d70cf068f
fabbernatvasvari <sze4fabber@vasvari.org>
2026-03-09 15:58:37 +0100

    jelszó megcsinálva

--- RoyalDelivery/Models/DbMysqlModels/User.cs
+++ RoyalDelivery/Models/DbMysqlModels/User.cs
@@ -15,7 +15,7 @@ public partial class User
 
     public string Email { get; set; } = string.Empty;
 
-    public string Password { get; set; } = string.Empty;
-
     public string Address { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
 }
